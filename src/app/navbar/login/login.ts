33244fe3fee79fe6e89056a5857aeff0b2543fa9
ASUS <ptharanan@gmail.com>
2025-10-12 15:18:52 +0530

    improve login-non functinoal

--- src/app/navbar/login/login.ts
+++ src/app/navbar/login/login.ts
@@ -1,26 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../../auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements AfterViewInit {
   adminName: string = "Admin";
   adminPassword: string = "Admin";
 
+  @ViewChild('username', { static: true }) usernameInput!: ElementRef;
+  @ViewChild('password', { static: true }) passwordInput!: ElementRef;
+
   constructor(private authService: Auth, private router: Router) { }
+
+  ngAfterViewInit() {
+    // பக்கம் load ஆனவுடன் username input-க்கு focus
+    this.usernameInput.nativeElement.focus();
+  }
+
+  handleKey(event: Event, field: string) {
+    const keyboardEvent = event as KeyboardEvent;
+    if (keyboardEvent.key === 'Enter') {
+      if (field === 'username') {
+        this.passwordInput.nativeElement.focus();
+      } else if (field === 'password') {
+        this.adminData(
+          this.usernameInput.nativeElement.value,
+          this.passwordInput.nativeElement.value
+        );
+      }
+    }
+  }
 
   adminData(User: string, Password: string) {
     if (User === this.adminName && Password === this.adminPassword) {
-      this.authService.login(); // ✅ triggers UI update
+      this.authService.login();
       this.router.navigate(['admin/dashboard']);
     } else {
       alert('Invalid Data');
-    }
 
+      this.usernameInput.nativeElement.value = '';
+      this.passwordInput.nativeElement.value = '';
+      this.usernameInput.nativeElement.focus();
+    }
   }
 }
